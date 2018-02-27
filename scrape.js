048285ade79aeb6bd5037dc01fbedc01da8d6597
Jerry Hampton <hamptonjt@hotmail.com>
2018-02-26 22:46:29 -0500

    adding twitter app functionality (just to comment it out)

--- scrape.js
+++ scrape.js
@@ -1,5 +1,34 @@
+/*
+ * Contacted the national pastime web site to attempt to get information about their data.
+ * Web site copyright is 2015 - so I'm not sure it's still being maintained.  Scraping the
+ * information I want for now.  Brittle, but working at the moment.
+ */
 let axios = require('axios')
 let cheerio = require('cheerio')
+
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('./twitter_keys.txt')
+});
+
+let consumer_key = ''
+let consumer_secret = ''
+let access_token = ''
+let access_token_secret = ''
+
+let counter = 0
+
+lineReader.on('line', function (line) {
+  // console.log('Line from file:', line);
+  counter += 1
+  switch (counter) {
+    case 1: consumer_key = line; break
+    case 2: consumer_secret = line; break
+    case 3: access_token = line; break
+    case 4: access_token_secret = line; break
+  }
+});
+
+
 
 const url = 'http://www.nationalpastime.com/site/index.php?action=baseball_team_search&baseball_team=New+York+Yankees'
 
@@ -29,6 +58,25 @@ axios.get(url).then(response => {
 
   // First thing is to test the data...
   console.log(data)
+  // const Twit = require('twit')
+
+  // const T = new Twit({
+  //   consumer_key:         consumer_key,
+  //   consumer_secret:      consumer_secret,
+  //   access_token:         access_token,
+  //   access_token_secret:  access_token_secret,
+  //   timeout_ms:           60 * 1000,
+  // });
+
+  // // Post a tweet
+  // T.post(
+  //   'statuses/update',
+  //   { status: `Today in ${data[0].year}: ${data[0].fact}` },
+  //   (err, data, response) => {
+  //     console.log(err, data, response);
+  //   }
+  // )
+
 
 }).catch(e => {
   console.error(e)
